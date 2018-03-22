5aea3b01a75cb7c0ac2b650dcc2f6f09c9d69074
Steven Crawford <scrawford@stsci.edu>
2018-03-22 15:35:40 -0400

    adding issue functionality

--- jirahub/jiraquery.py
+++ jirahub/jiraquery.py
@@ -1,9 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # This module implements the JiraQuery class.
 
-from jira import JIRA
+from jira import JIRA, JIRAError
 
-__all__=['JiraQuery']
+__all__ = ['JiraQuery']
 
 
 class JiraQuery(object):
@@ -16,8 +16,8 @@ class JiraQuery(object):
         Parameters
         ----------
         jira: str
-            Name of a repository to track and update the issues.  The name of the repository
-            should be in the form of 'url/project/name'
+            Name of a repository to track and update the issues.  The name of
+            the repository should be in the form of 'url/project/name'
 
         key: str
             Authentification token for the current user
@@ -34,8 +34,29 @@ class JiraQuery(object):
 
         # authenticate with github
         if key:
-           self.jira = JIRA(site, oauth=key)
+            self.jira = JIRA(site, oauth=key)
         elif user is not None:
-           self.jira = JIRA(site, basic_auth=(user, password))
+            self.jira = JIRA(site, basic_auth=(user, password))
         else:
-            self.jira = JIRA(site)
+             self.jira = JIRA(site)
+
+    @property
+    def issue(self):
+        return self._issue
+
+    @issue.setter
+    def issue(self, issue_id):
+        try:
+            self._issue = self.jira.issue(issue_id)
+        except JIRAError:
+            self._issue = None
+
+    def add_comment(self, comment):
+        """Add a comment to an issue.  
+
+        Parameters
+        ----------
+        comment: str
+            Comment to be added to an issue
+        """
+        self.jira.add_comment(self.issue, comment)
